9471f7b69bb56326ec210ab1c6342dd5e7a8a1c6
deucedhunter <deucedhunter@gmail.com>
2024-12-13 18:52:00 +0100

    Remove duplicated headers on pages

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ df = pd.read_csv("topics.csv", sep=",")
 for index, row in df.iterrows():
     pages = int(row['Pages'])
     topic = row['Topic']
-    for page in range(pages):
+    pdf.add_page()
+    pdf.set_font(family="Arial", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=topic,
+             align="L", ln=1)
+    pdf.line(10,21,200,21)
+    for page in range(pages-1):
         pdf.add_page()
-        pdf.set_font(family="Arial", style="B", size=24)
-        pdf.set_text_color(100,100,100)
-        pdf.cell(w=0, h=12, txt=topic,
-                 align="L", ln=1)
-        pdf.line(10,21,200,21)
 
 
 pdf.output("output.pdf")
